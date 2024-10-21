e3f3e536ad0eeac281037e2c4ac0919777fd768b
sstefanov <84671344+sstefanov19@users.noreply.github.com>
2024-10-21 19:52:36 +0300

    more fixing

--- src/app/classes/page.tsx
+++ src/app/classes/page.tsx
@@ -15,6 +15,7 @@ interface Subject {
 declare module 'next-auth' {
   interface Session {
     user: {
+      id: number;
       name?: string | null;
       email?: string | null;
       image?: string | null;
